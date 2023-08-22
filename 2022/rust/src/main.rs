a6899024efff0d265114eae6c25743c18762bf65
christophe croese <christophe.croese@thalesgroup.com>
2023-08-22 12:03:36 +0200

    day1 working

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -7,5 +7,30 @@ fn main() {
     println!("Hello, world!");
 
     let contents = fs::read_to_string(PATH).expect("Should have been able to read a file");
-    println!("{}", contents);
+    //println!("{}", contents);
+    let mut calories: usize = 0;
+    let mut elf_counter: usize = 0;
+    let mut max_calories: usize = 0;
+
+    for line in contents.lines() {
+        let calorie: usize = match line.trim().parse() {
+            Ok(cal) => cal,
+            Err(_) => {
+                //previous elf:
+                println!("Elf n°{} is carrying {} calories (max calories for now: {})",elf_counter, calories, max_calories);
+
+                //current elf: updating counters
+                elf_counter += 1;
+                if calories > max_calories{
+                    max_calories = calories;
+                }
+                calories = 0;
+                continue;
+            },
+        };
+        calories += calorie;
+        //println!("Found {} cals", calorie);
+    }
+    println!("Last elf n°{} is carrying {} calories (max calories for now : {})", elf_counter, calories, max_calories);
 }
+
